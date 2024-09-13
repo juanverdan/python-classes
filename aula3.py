2d4fb2df0a9f31c9bf9458149187109cff88425c
Juan Verdan dos Santos <indianoverdan@gmail.com>
2024-09-13 20:48:57 -0300

    aula nova

---
+++ aula3.py
@@ -0,0 +1,18 @@
+# STRINGS - INTRODUÇÃO AOS TIPOS DE DADOS
+# str -> string -> texto
+# Strings são textos dentro de aspas
+
+#ASPAS SIMPLES
+print('Mariano')
+print(1, 'Luis "Mariano"')
+
+#ASPAS DUPLAS
+print("Mariano")
+print(2, "Luis 'Mariano'")
+
+#ESCAPE
+print("Mariano \"João")
+
+#r
+print(r"Mariano \"João\"")
+
