e7e4031da4356f5e0a814313c5aae6065b4a804d
Juan Verdan dos Santos <indianoverdan@gmail.com>
2024-09-17 14:26:45 -0300

    aula nova

---
+++ aula7_var.py
@@ -0,0 +1,12 @@
+#Variáveis são usadas para salvar algo na memória
+# o = serve para atribuição
+
+nome_completo = 'Juan Verdan dos Santos'
+soma = 2+2
+print(nome_completo, soma)
+
+nome = 'Luiz'
+idade = 30
+maior_de_idade = idade >= 18
+print('Nome:', nome)
+print('É maior?', maior_de_idade)
---
+++ aula8_exercvar.py
@@ -0,0 +1,13 @@
+nome = 'Juan'
+sobrenome = 'Verdan'
+idade = 23
+ano_nascimento = 2024 - idade
+maior_de_idade = 18 >= idade
+altura_metros = 1.90
+
+print('Nome', nome)
+print('Sobrenome', sobrenome)
+print('Idade', idade)
+print('Ano de Nascimento', ano_nascimento)
+print('É maior de idade?', maior_de_idade)
+print('Altura em metros:', altura_metros)
