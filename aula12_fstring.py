b75a9017e2b0289d9a36f72499d7cd38c4bf2144
Juan Verdan dos Santos <indianoverdan@gmail.com>
2024-09-18 15:29:38 -0300

    :memo: aula nova

---
+++ aula12_fstring.py
@@ -0,0 +1,7 @@
+nome = 'Juan'
+altura = 1.86
+peso = 83
+imc = peso / (altura * altura)
+
+linha_1 = f'{nome} tem {altura:.2f} de altura e pesa {peso} quilos e seu imc é: {imc}'
+print(linha_1)
---
+++ exercicio1_calc_imc.py
@@ -0,0 +1,6 @@
+Nome = 'Juan'
+altura = 1.86
+peso = 83
+imc = peso / (altura * altura)
+
+print(imc)
