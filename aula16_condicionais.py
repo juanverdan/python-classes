829c28934328d72a9b5ac8a9a2322e9bc7990a48
Juan Verdan dos Santos <indianoverdan@gmail.com>
2024-09-20 21:00:54 -0300

    relembrando aula de ontem

--- aula16_condicionais.py
+++ aula16_condicionais.py
@@ -1,5 +1,5 @@
 #if elif else
-
+'''
 entrada = input('Você quer entrar ou sair? ')
 
 if entrada == 'entrar':     
@@ -10,3 +10,24 @@ elif entrada == 'sair':
 
 else:
     print('Você não digitou nenhuma das opções') 
+
+'''
+
+
+#aparentemente, se eu colocar esse int, se eu colocar string, o código nem continua, terei que tomar cuidado!
+nota1 = int(input('Qual sua nota 1? '))
+nota2 = int(input('Qual sua nota 2? '))
+nota3 = int(input('Qual sua nota 3? '))
+nota4 = int(input('Qual sua nota 4? '))
+
+nota_geral = (nota1 + nota2 + nota3 + nota4)
+print(nota_geral)
+
+if nota_geral < 20:
+    print('Você reprovou')
+
+elif nota_geral >= 20:
+    print('Parabéns!')
+
+else:
+    print('Digite novamente bro')
