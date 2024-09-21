f35245cb91a1982093f0a942f16df29d6afbaec3
Juan Verdan dos Santos <indianoverdan@gmail.com>
2024-09-21 11:15:01 -0300

    Criação da Pasta Basica

---
+++ basico/aula17_condicionais2.py
@@ -0,0 +1,16 @@
+a = False
+b = False
+c = True
+d = False
+
+if a:
+    print('Código para a')
+    print('Código para a')
+elif b:
+    print('Código para b')
+elif c:
+    print('Codigo para c')
+elif d:
+    print('Código para d')
+else:
+    print('Nenhuma')
