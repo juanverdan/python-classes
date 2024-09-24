d7281a9b1fbca94320dbd45800ce9b9ab2f2e854
Juan Verdan dos Santos <indianoverdan@gmail.com>
2024-09-23 21:18:00 -0300

    aula sobre operadores

---
+++ basico/aula19_operad_comp.py
@@ -0,0 +1,17 @@
+'''
+Operadores de comparação (relacionais)
+>   maior            3 > 2
+>=  maior ou igual   2 >= 2
+<   menor            2 < 3
+<=  menor ou igual   2 <= 2
+==  igual           'a' == 'a'
+!=  diferente       '1' = 1
+'''
+maior = 2 > 1
+maior_igual = 2 >= 2
+menor = 1 < 2
+menor_igual = 2 <= 2
+igual = 'a' == 'a'
+dif = '1' != 1
+
+print(dif)
