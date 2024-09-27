4186495c457b2927e17c51f881366a7f33583607
Juan Verdan dos Santos <indianoverdan@gmail.com>
2024-09-26 21:57:32 -0300

    :memo: In e Not In

---
+++ basico/aula23_not.py
@@ -0,0 +1,12 @@
+# Operador lógico "not"
+# Usado para inverter expressões
+# not True = false || not False = True
+
+senha = input('Senha: ')
+
+if not senha :
+    print('Você não digitou nada')
+
+
+#print(not True) # false
+#print(not False) #true
---
+++ basico/aula24_in_notIn.py
@@ -0,0 +1,12 @@
+"""
+Operadores in e not in
+Strings são iteráveis
+ 0 1 2 3 4 5
+ O t a v i o
+-6-5-4-3-2-1
+"""
+nome = 'Rebeca'
+print(nome[2])     #isso é uma iteração
+print('c' in nome)
+print('z' in nome)
+print('w' not in nome)
